79134cd0b0812aece8afcca0901b8f727468c988
Franklin van de Meent <fr@nkl.in>
2015-05-18 07:11:18 +0200

    Replaced XML with JSON
    Requires vnstat >= v1.13
    <https://github.com/vergoh/vnstat>

--- vnstat-dumpdb.js
+++ vnstat-dumpdb.js
@@ -1,5 +1,4 @@
 var exec = require ('child_process') .exec;
-var xml2json = require ('node-xml2json');
 
 var set = {
   bin: 'vnstat',
@@ -33,98 +32,35 @@ function getConfig (callback) {
   });
 }
 
-// parse interface
-function fixInterface (iface) {
-  var i;
-
-  // id
-  if (iface.id && iface.id [0]) {
-    iface.id = iface.id [0];
-  }
-
-  // days
-  if (iface.traffic.days.day instanceof Array) {
-    for (i = 0; i < iface.traffic.days.day.length; i++) {
-      iface.traffic.days.day [i] .rx = iface.traffic.days.rx [i] || iface.traffic.days.day [i] .rx || 0;
-      iface.traffic.days.day [i] .tx = iface.traffic.days.tx [i] || iface.traffic.days.day [i] .tx || 0;
-    }
-    iface.traffic.days = iface.traffic.days.day;
-  } else {
-    var day = iface.traffic.days.day;
-    day.rx = iface.traffic.days.rx || iface.traffic.days.day.rx || 0;
-    day.tx = iface.traffic.days.tx || iface.traffic.days.day.tx || 0;
-    iface.traffic.days = [day];
-  }
-
-  // months
-  if (iface.traffic.months.month instanceof Array) {
-    for (i = 0; i < iface.traffic.months.month.length; i++) {
-      iface.traffic.months.month [i] .rx = iface.traffic.months.rx [i] || iface.traffic.months.month [i] .rx || 0;
-      iface.traffic.months.month [i] .tx = iface.traffic.months.tx [i] || iface.traffic.months.month [i] .tx || 0;
-    }
-    iface.traffic.months = iface.traffic.months.month;
-  } else {
-    var month = iface.traffic.months.month;
-    month.rx = iface.traffic.months.rx || 0;
-    month.tx = iface.traffic.months.tx || 0;
-    iface.traffic.months = [month];
-  }
-
-  // hours
-  if (iface.traffic.hours.hour instanceof Array) {
-    iface.traffic.hours = iface.traffic.hours.hour;
-  } else {
-    iface.traffic.hours = [iface.traffic.hours.hour];
-  }
-
-  // tops
-  if (!iface.traffic.tops.top) {
-    iface.traffic.tops = [];
-  } else {
-    iface.traffic.tops = iface.traffic.tops.top;
-  }
-
-  return iface;
-}
-
-// Get database
+// Get stats database
 function getDatabase (iface, callback) {
-  exec (set.bin +' -i '+ iface +' --dumpdb --xml', function (err, xml, stderr) {
+  var i;
+  var doIface = iface ? ' -i '+ iface : '';
+
+  exec (set.bin + doIface +' --dumpdb --json', function (err, json, stderr) {
     var error = null;
     if (err instanceof Error) {
       error = new Error (stderr.trim ());
-      callback (error);
+      error.details = json;
+      return callback (error);
     }
 
     try {
-      xml = xml2json.parser (xml.trim ());
-      if (!xml.vnstat) {
-        error = new Error ('invalid xml');
-        error.details = xml;
-      } else {
-        if (xml.vnstat.interface [0]) {
-          // multiple
-          var ifaces = {};
-          var i;
-          for (i = 0; i < xml.vnstat.interface.length; i++) {
-            iface = fixInterface (xml.vnstat.interface [i]);
-            ifaces [iface.id || iface.nick] = iface;
+      json = JSON.parse (json);
+      if (iface) {
+        for (i = 0; i < json.interfaces.length; i++) {
+          if (json.interfaces [i] .id === iface) {
+            return callback (null, json.interfaces [i]);
           }
-          xml = ifaces;
-        } else {
-          // just one iface
-          xml = fixInterface (xml.vnstat.interface);
         }
+      } else {
+        callback (null, json.interfaces);
       }
     }
     catch (e) {
-      error = new Error ('can not process');
-      error.details = xml || '';
-      error.error = e;
-      xml = null;
-    }
-    finally {
-      callback (error, xml);
+      error = new Error ('invalid data');
+      error.details = json;
+      callback (error);
     }
   });
 }

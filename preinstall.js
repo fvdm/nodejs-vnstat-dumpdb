e5e1d95ebd19904195ceab7e9916300de6e9f78c
Franklin van de Meent <fr@nkl.in>
2016-04-06 23:03:08 +0200

    Clean up preinstall.js

--- preinstall.js
+++ preinstall.js
@@ -3,13 +3,17 @@ var exec = require ('child_process') .exec;
 console.log ('Checking vnStat requirement...');
 
 exec ('vnstat --version', function (err, res, stderr) {
-  if (err) { throw err; }
+  if (err) {
+    throw err;
+  }
+
   res.replace (/^vnStat (\d+)\.(\d+) /, function (s, major, minor) {
     if (major >= 1 && minor >= 13) {
       console.log ('vnStat correct');
-    } else {
-      console.log ('Wrong vnStat version: requires >= v1.13, but v'+ major +'.'+ minor +' installed');
-      process.exit (1);
+      return;
     }
+
+    console.log ('Wrong vnStat version: requires >= v1.13, but v' + major + '.' + minor + ' installed');
+    process.exit (1);
   });
 });

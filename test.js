dbb8681f9feb4ea290e350361fdf61da5669305c
Franklin van de Meent <fr@nkl.in>
2016-04-06 23:04:21 +0200

    Test: rewrite script to dotest module

--- test.js
+++ test.js
@@ -1,109 +1,54 @@
-var util = require ('util');
+var dotest = require ('dotest');
+var app = require ('./');
 
 // Setup
 // $ NODE_APP_IFACE=eth0 npm test
-var app = require ('./') ();
+var vnstat = app ();
 
 
-// handle exits
-var errors = 0;
-process.on ('exit', function () {
-  if (errors === 0) {
-    console.log ('\n\033[1mDONE, no errors.\033[0m\n');
-    process.exit (0);
-  } else {
-    console.log ('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m\n');
-    process.exit (1);
-  }
-});
-
-// prevent errors from killing the process
-process.on ('uncaughtException', function (err) {
-  console.log ();
-  console.error (err.stack);
-  console.log ();
-  errors++;
-});
-
-// Queue to prevent flooding
-var queue = [];
-var next = 0;
-
-function doNext () {
-  next++;
-  if (queue [next]) {
-    queue [next] ();
-  }
-}
-
-// doTest( passErr, 'methods', [
-//   ['feeds', typeof feeds === 'object']
-// ])
-function doTest (err, label, tests) {
-  if (err instanceof Error) {
-    console.error (label +': \033[1m\033[31mERROR\033[0m\n');
-    console.error (util.inspect (err, {depth: 10, colors: true}));
-    console.log ();
-    console.error (err.stack);
-    console.log ();
-    errors++;
-  } else {
-    var testErrors = [];
-    for (var i = 0; i < tests.length; i++) {
-      if (tests [i] [1] !== true) {
-        testErrors.push (tests [i] [0]);
-        errors++;
-      }
-    }
-
-    if (testErrors.length === 0) {
-      console.log (label +': \033[1m\033[32mok\033[0m');
-    } else {
-      console.error (label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join (', ') +')');
-    }
-  }
-
-  doNext ();
-}
-
-queue.push (function () {
+dotest.add ('getConfig', function (test) {
   app.getConfig (function (err, data) {
-    doTest (err, 'getConfig', [
-      ['DatabaseDir', !!data.DatabaseDir],
-      ['Interface', !!data.Interface]
-    ]);
+    test (err)
+      .isObject ('fail', 'data', data)
+      .isNotEmpty ('fail', 'data.DatabaseDir', data && data.DatabaseDir)
+      .isNotEmpty ('fail', 'data.Interface', data && data.Interface)
+      .done ();
   })
 });
 
-queue.push (function () {
+dotest.add ('getStats iface', function (test) {
   app.getStats (process.env.NODE_APP_IFACE || 'eth0', function (err, data) {
-    doTest (err, 'getStats iface', [
-      ['data type', typeof data === 'object'],
-      ['id', typeof data.id === 'string'],
-      ['days type', data.traffic.days && data.traffic.days instanceof Array],
-      ['days item', data.traffic.days && typeof data.traffic.days [0] .rx === 'number']
-    ]);
+    var days = data && data.traffic && data.traffic.days;
+    var rx = days && days [0] && days [0] .rx;
+
+    test (err)
+      .isObject ('fail', 'data', data)
+      .isString ('fail', 'data.id', data && data.id)
+      .isObject ('fail', 'data.traffic', data && data.traffic)
+      .isArray ('fail', 'data.traffic.days', days)
+      .isObject ('fail', 'data.traffic.days[0]', days && days [0])
+      .isNumber ('fail', 'data.traffic.days[0].rx', rx)
+      .done ();
   });
 });
 
-queue.push (function () {
+dotest.add ('getStats all', function (test) {
   app.getStats (function (err, data) {
-    doTest (err, 'getStats all', [
-      ['data type', typeof data === 'object' && data instanceof Array]
-    ]);
+    test (err)
+      .isArray ('fail', 'data', data)
+      .done ();
   });
 });
 
-queue.push (function () {
+dotest.add ('getStats error', function (test) {
   app.getStats ('unreal-iface', function (err) {
-    doTest (null, 'getStats error', [
-      ['type', err instanceof Error],
-      ['message', err.message === 'invalid interface']
-    ]);
+    test ()
+      .isError ('fail', 'err', err)
+      .isExactly ('fail', 'err.message', 'invalid interface')
+      .done ();
   });
 });
 
 
 // Start the tests
-console.log ('Running tests...\n');
-queue [0] ();
+dotest.run ();
